57ab7feb5756410a675e44f28bbfec26a7b24a97
Katerina881 <mr.alodov@mail.ru>
2020-01-17 19:27:39 +0400

    fikc

---
+++ TP_LABS/FormParkingBus.designer.cs
@@ -0,0 +1,160 @@
+﻿namespace TP_LABS
+{
+    partial class FormParkingBus
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxParking = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBoxBus = new System.Windows.Forms.PictureBox();
+            this.buttonTake = new System.Windows.Forms.Button();
+            this.maskedTextBoxPlace = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonCreateBus = new System.Windows.Forms.Button();
+            this.buttonCreateCommonBus = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxParking
+            // 
+            this.pictureBoxParking.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxParking.Name = "pictureBoxParking";
+            this.pictureBoxParking.Size = new System.Drawing.Size(737, 426);
+            this.pictureBoxParking.TabIndex = 0;
+            this.pictureBoxParking.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBoxBus);
+            this.groupBox1.Controls.Add(this.buttonTake);
+            this.groupBox1.Controls.Add(this.maskedTextBoxPlace);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(755, 262);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(116, 175);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBoxBus
+            // 
+            this.pictureBoxBus.Location = new System.Drawing.Point(6, 85);
+            this.pictureBoxBus.Name = "pictureBoxBus";
+            this.pictureBoxBus.Size = new System.Drawing.Size(104, 84);
+            this.pictureBoxBus.TabIndex = 4;
+            this.pictureBoxBus.TabStop = false;
+            // 
+            // buttonTake
+            // 
+            this.buttonTake.Location = new System.Drawing.Point(16, 58);
+            this.buttonTake.Name = "buttonTake";
+            this.buttonTake.Size = new System.Drawing.Size(85, 21);
+            this.buttonTake.TabIndex = 3;
+            this.buttonTake.Text = "Забрать";
+            this.buttonTake.UseVisualStyleBackColor = true;
+            this.buttonTake.Click += new System.EventHandler(this.buttonTakeBus_Click);
+            // 
+            // maskedTextBoxPlace
+            // 
+            this.maskedTextBoxPlace.Location = new System.Drawing.Point(66, 32);
+            this.maskedTextBoxPlace.Name = "maskedTextBoxPlace";
+            this.maskedTextBoxPlace.Size = new System.Drawing.Size(35, 20);
+            this.maskedTextBoxPlace.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Место";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Забрать автобус";
+            // 
+            // buttonCreateBus
+            // 
+            this.buttonCreateBus.Location = new System.Drawing.Point(755, 39);
+            this.buttonCreateBus.Name = "buttonCreateBus";
+            this.buttonCreateBus.Size = new System.Drawing.Size(125, 52);
+            this.buttonCreateBus.TabIndex = 2;
+            this.buttonCreateBus.Text = "Припарковать автобус";
+            this.buttonCreateBus.UseVisualStyleBackColor = true;
+            this.buttonCreateBus.Click += new System.EventHandler(this.buttonSetCommonBus_Click);
+            // 
+            // buttonCreateCommonBus
+            // 
+            this.buttonCreateCommonBus.Location = new System.Drawing.Point(755, 97);
+            this.buttonCreateCommonBus.Name = "buttonCreateCommonBus";
+            this.buttonCreateCommonBus.Size = new System.Drawing.Size(125, 53);
+            this.buttonCreateCommonBus.TabIndex = 3;
+            this.buttonCreateCommonBus.Text = "Припарковать двухэтажный автобус";
+            this.buttonCreateCommonBus.UseVisualStyleBackColor = true;
+            this.buttonCreateCommonBus.Click += new System.EventHandler(this.buttonSetBus_Click);
+            // 
+            // FormParkingBus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.buttonCreateCommonBus);
+            this.Controls.Add(this.buttonCreateBus);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBoxParking);
+            this.Name = "FormParkingBus";
+            this.Text = "FormParkingBus";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxParking)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBus)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxParking;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBoxBus;
+        private System.Windows.Forms.Button buttonTake;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxPlace;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonCreateBus;
+        private System.Windows.Forms.Button buttonCreateCommonBus;
+    }
+}
