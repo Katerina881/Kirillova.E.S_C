52c2fd97e053dfa8058907af6a40f2d8c8abf5df
Katerina881 <mr.alodov@mail.ru>
2020-01-18 10:36:10 +0400

    Fikc2

--- TP_LABS/FormBus.Designer.cs
+++ TP_LABS/FormBus.Designer.cs
@@ -34,7 +34,7 @@
             this.buttonDown = new System.Windows.Forms.Button();
             this.buttonLeft = new System.Windows.Forms.Button();
             this.buttonRight = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonCreateCommon = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBus)).BeginInit();
             this.SuspendLayout();
             // 
@@ -105,22 +105,22 @@
             this.buttonRight.UseVisualStyleBackColor = true;
             this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
             // 
-            // button1
+            // buttonCreateCommon
             // 
-            this.button1.Location = new System.Drawing.Point(259, 10);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(214, 28);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Создать обычный автобус";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.buttonCreateCommon_Click);
+            this.buttonCreateCommon.Location = new System.Drawing.Point(259, 10);
+            this.buttonCreateCommon.Name = "buttonCreateCommon";
+            this.buttonCreateCommon.Size = new System.Drawing.Size(214, 28);
+            this.buttonCreateCommon.TabIndex = 6;
+            this.buttonCreateCommon.Text = "Создать обычный автобус";
+            this.buttonCreateCommon.UseVisualStyleBackColor = true;
+            this.buttonCreateCommon.Click += new System.EventHandler(this.buttonCreateCommon_Click);
             // 
             // FormBus
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 461);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonCreateCommon);
             this.Controls.Add(this.buttonRight);
             this.Controls.Add(this.buttonLeft);
             this.Controls.Add(this.buttonDown);
@@ -143,7 +143,7 @@
         private System.Windows.Forms.Button buttonDown;
         private System.Windows.Forms.Button buttonLeft;
         private System.Windows.Forms.Button buttonRight;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonCreateCommon;
     }
 }
 
